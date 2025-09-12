0d8aad4176da565a37520e4ffa82f19b88387a6a
Ajimsha1080 <147695569+Ajimsha1080@users.noreply.github.com>
2025-09-12 19:57:28 +0530

    Update cp_dataset.py

--- cp_dataset.py
+++ cp_dataset.py
@@ -1,78 +1,49 @@
+import os
 import os.path as osp
+from torch.utils.data import Dataset
 from PIL import Image
-import torch
-import torch.utils.data as data
 import torchvision.transforms as transforms
 
 
-class CPDataset(data.Dataset):
+class CPDataset(Dataset):
     def __init__(self, opt):
         super(CPDataset, self).__init__()
         self.opt = opt
-        self.root = opt.dataroot
-        self.datamode = opt.datamode
-        self.stage = opt.stage
-        self.data_list = opt.data_list
+        self.data_path = opt.dataroot
+        self.fine_width = opt.fine_width
+        self.fine_height = opt.fine_height
 
-        # Read pairs (image, cloth)
-        im_names = []
-        c_names = []
-        with open(osp.join(self.root, self.data_list), 'r') as f:
-            for line in f.readlines():
-                im_name, c_name = line.strip().split()
-                im_names.append(im_name)
-                c_names.append(c_name)
-        self.im_names = im_names
-        self.c_names = c_names
+        # Load pairs file (e.g., test_pairs.txt)
+        pair_path = osp.join(self.data_path, opt.data_list)
+        with open(pair_path, 'r') as f:
+            self.pairs = [line.strip().split() for line in f.readlines()]
 
-        # Transforms
-        self.image_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5),
-                                 (0.5, 0.5, 0.5))
-        ])
-
-        # For parsing/shape (grayscale)
-        self.mask_transform = transforms.Compose([
+        # Basic transforms
+        self.transform = transforms.Compose([
+            transforms.Resize((self.fine_height, self.fine_width)),
             transforms.ToTensor(),
             transforms.Normalize((0.5,), (0.5,))
         ])
 
-        self.fine_height = opt.fine_height
-        self.fine_width = opt.fine_width
-
     def __getitem__(self, index):
-        im_name = self.im_names[index]
-        c_name = self.c_names[index]
+        im_name, c_name = self.pairs[index]
 
         # Person image
-        im_path = osp.join(self.root, 'image', im_name)
-        im = Image.open(im_path).convert('RGB')
-        image = self.image_transform(im)
+        im_path = osp.join(self.data_path, self.opt.datamode, "image", im_name)
+        image = Image.open(im_path).convert("RGB")
+        image = self.transform(image)
 
         # Cloth image
-        c_path = osp.join(self.root, 'cloth', c_name)
-        c = Image.open(c_path).convert('RGB')
-        cloth = self.image_transform(c)
+        c_path = osp.join(self.data_path, self.opt.datamode, "cloth", c_name)
+        cloth = Image.open(c_path).convert("RGB")
+        cloth = self.transform(cloth)
 
-        # Cloth mask (binary)
-        cm_path = osp.join(self.root, 'cloth-mask', c_name)
-        cm = Image.open(cm_path).convert('L')
-        cloth_mask = self.mask_transform(cm)
-
-        # Parsing / shape original (grayscale)
-        parse_shape_ori_path = osp.join(self.root, 'image-parse', im_name.replace('.jpg', '_shape.png'))
-        parse_shape_ori = Image.open(parse_shape_ori_path).convert('L')
-        shape_ori = self.mask_transform(parse_shape_ori)
-
-        # Return dictionary
         return {
-            'image': image,
-            'cloth': cloth,
-            'cloth_mask': cloth_mask,
-            'shape_ori': shape_ori,
-            'im_name': im_name,
-            'c_name': c_name
+            "image": image,
+            "cloth": cloth,
+            "im_name": im_name,
+            "c_name": c_name,
         }
 
-   
+    def __len__(self):
+        return len(self.pairs)   # <-- This was missing
